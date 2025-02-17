0eb70ec17c735f11c6bf0f24d1c64736628b1d2e
kevwin717 <kevinn0308@gmail.com>
2025-02-17 17:00:38 -0600

    Update header layout and styling with logo and navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,19 @@ function Header() {
     console.log('Header component rendered'); //debug
     return (
         <header>
-            <h1>Kevin Nguyen</h1>
+            <div className='logo-content'>
+                <img src='/images/logo.jpeg'/>
+                <h1>Kevin Nguyen</h1>
+            </div>
             <nav>
-                <a href="#about">About</a>
-                <a href="#skills">Skills</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
-                <a href="https://github.com/KevWin717" target="_blank" rel="noopener noreferrer">Github</a>
-                <a href="https://linkedin.com/in/kevinnguyen717" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <ul className='nav-links'>
+                    <a href="#about">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
+                    <a href="https://github.com/KevWin717" target="_blank" rel="noopener noreferrer">Github</a>
+                    <a href="https://linkedin.com/in/kevinnguyen717" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                </ul>
             </nav>
         </header>
     );
